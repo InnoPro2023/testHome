02956dcb0e73e11807ae9be2d8b0d7c8038b5c48
DESKTOP-ERMH4GV\DELL <scottnguyen2411@gmail.com>
2023-04-18 15:01:14 +0700

    Minor update

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -7,7 +7,6 @@ import { Nopage } from './components/Nopage';
 function App() {
   return (
     <>
-      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='About' element={<About/>}/>
